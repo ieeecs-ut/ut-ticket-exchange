f061eb457b2f7b72faf6ff2134fdb2c1a65280a0
anuvgupta <me@anuv.me>
2022-02-28 19:22:17 -0800

    add basic model functions for user

--- backend/modules/db.js
+++ backend/modules/db.js
@@ -43,22 +43,76 @@ var api = {
             }
         });
     },
-    validate_login: (email, password, resolve) => {
-        mongo_api.collection('user').find({
+    // user
+    create_user: (email, new_password, resolve) => {
+        var ts_now = (new Date()).getTime();
+        mongo_api.collection('user').insertOne({
             email: email,
-            password: password
-        }, (error1, cursor1) => {
+            password: new_password,
+            ts_updated: ts_now,
+            ts_created: ts_now,
+        }, (error1, result1) => {
             if (error1) {
-                err(`error validating login for user with email ${email}`, error1.message ? error1.message : error1);
+                err(`error creating user with email ${email}`, error1.message ? error1.message : error1);
                 resolve(null);
             } else {
-                cursor1.count().then(c => {
-                    if (c <= 0) {
-                        resolve(false);
-                    } else {
-                        resolve(true);
-                    }
-                });
+                resolve(true);
+            }
+        });
+    },
+    get_user: (id, resolve) => {
+        mongo_api.collection('project').findOne({ '_id': mongo_oid(id) }, (e, result1) => {
+            if (e) {
+                err(`error finding user ${id}`, e.message ? e.message : e);
+                resolve(false, e);
+            } else {
+                if (result1) resolve(true, result1);
+                else resolve(null, result1);
+            }
+        });
+    },
+    update_user: (id, update, resolve) => {
+        var ts_now = (new Date()).getTime();
+        mongo_api.collection('user').findOne({ _id: mongo_oid(id) }, (e, result1) => {
+            if (e) {
+                err(`error finding user ${id}`, e.message ? e.message : e);
+                resolve(false, e);
+            } else {
+                if (!result1) resolve(null, result1);
+                else {
+                    if (!update.hasOwnProperty('ts_updated'))
+                        update.ts_updated = ts_now;
+                    mongo_api.collection('user').updateOne({ _id: mongo_oid(id) }, {
+                        $set: update
+                    }, (e2, result2) => {
+                        if (e2) {
+                            err(`error updating project ${id}`, e2.message ? e2.message : e2);
+                            resolve(false, e2);
+                        } else {
+                            mongo_api.collection('user').findOne({ _id: mongo_oid(id) }, (e3, result3) => {
+                                if (e3) {
+                                    err(`error finding user ${id} after update`, e3.message ? e3.message : e3);
+                                    resolve(false, e3);
+                                } else {
+                                    if (!result3) resolve(null, result3);
+                                    else resolve(true, result3);
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    },
+    delete_user: (id, resolve) => {
+        mongo_api.collection('user').deleteOne({ _id: mongo_oid(id) }, (e, coll1) => {
+            if (e) {
+                err(`error finding user ${id}`, e.message ? e.message : e);
+                resolve(false, e);
+            } else {
+                if (!coll1 || coll1.result.n != 1)
+                    resolve(null, coll1);
+                else resolve(true, coll1);
             }
         });
     },
@@ -80,19 +134,32 @@ var api = {
             }
         });
     },
-    create_user: (email, new_password, resolve) => {
-        mongo_api.collection('user').insertOne({
+    validate_login: (email, password, resolve) => {
+        mongo_api.collection('user').find({
             email: email,
-            password: new_password
-        }, (error1, result1) => {
+            password: password
+        }, (error1, cursor1) => {
             if (error1) {
-                err(`error creating user with email ${email}`, error1.message ? error1.message : error1);
+                err(`error validating login for user with email ${email}`, error1.message ? error1.message : error1);
                 resolve(null);
             } else {
-                resolve(true);
+                cursor1.count().then(c => {
+                    if (c <= 0) {
+                        resolve(false);
+                    } else {
+                        resolve(true);
+                    }
+                });
             }
         });
-    }
+    },
+
+    // buy_order
+
+    // sell_order
+
+    // game
+
 };
 
 
